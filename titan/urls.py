309b39944968d29f4165570c945d9d43a5e6c091
archi <archipurohit2005@gmail.com>
2026-08-26 00:34:53 +0530

    forgot password function

--- titan/urls.py
+++ titan/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+
 
 admin.site.site_header = "Titanic Admin Panel"
 admin.site.site_title = "Titanic Admin"
@@ -27,4 +29,16 @@ urlpatterns = [
     path('', include('orders.urls')),
     path('', include('agents.urls')),
     path('', include('customers.urls')),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]

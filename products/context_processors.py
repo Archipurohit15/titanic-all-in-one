f0672a540abc78949793b23fd7df9b44a24546ad
archi <archipurohit2005@gmail.com>
2026-09-12 03:46:00 +0530

    changed two categories

--- products/context_processors.py
+++ products/context_processors.py
@@ -4,8 +4,8 @@ from .models import Categories
 def featured_categories(request):
     return {
         'featured_categories': {
-            'fmcg': Categories.objects.filter(parent=None, name__in=['FMCG', 'Fmcg', 'Personal Care & Household', 'Personal Care & Household']).first(),
-            'groceries': Categories.objects.filter(parent=None, name__in=['Groceries', 'Grocery']).first(),
+            'industrial': Categories.objects.filter(parent=None, name__in=['Industrial', 'FMCG', 'Fmcg', 'Personal Care & Household']).first(),
+            'tools': Categories.objects.filter(parent=None, name__in=['Tools', 'Groceries', 'Grocery']).first(),
             'electronics': Categories.objects.filter(parent=None, name__in=['Electronics']).first(),
             'electricals': Categories.objects.filter(parent=None, name__in=['Electrical', 'Electricals']).first(),
         }

5dbaba37394f48c1dacd088e29fd7e6b04b49684
archi <archipurohit2005@gmail.com>
2026-09-25 22:35:35 +0530

    sitemap debug x2

--- titan/settings.py
+++ titan/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!v1khllh@u0vuqpjdzac=izstoa+7rqiw5)lhjmgr7tska52q+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
